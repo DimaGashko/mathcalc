d13fbeed7c823dc12c92026217813bf4b52832cf
DimaGashko <dgashko@mail.ua>
2019-02-11 11:29:00 +0200

    matrix-mul-numBase

---
+++ src/pages/matrix-mul-num/matrix-mul-num.ts
@@ -0,0 +1,49 @@
+import '../templates/calcPage/calcPage';
+import './matrix-mul-num.sass';
+
+import MatrixDom from '../../components/MatrixDom/MatrixDom';
+import matrixMulNum from '../../calcFunctions/matrixMulNum';
+const workspace = document.querySelector('.calc-workspace');
+
+const els = {
+   matrixA: workspace.querySelector('.calc__matrixA'),
+   matrixB: workspace.querySelector('.calc__matrixB'),
+}
+
+console.log('Matrix multiplication');
+ 
+let num = 5;
+
+const matrixA = new MatrixDom({
+   title: 'Matrix A',
+});
+const matrixB = new MatrixDom({
+   title: `A * ${num}`,
+   disabled: true,
+});
+
+els.matrixA.appendChild(matrixA.root);
+els.matrixB.appendChild(matrixB.root);
+
+calc();
+
+matrixA.addEvent('change-data', () => {
+   calc();
+});
+
+
+matrixA.addEvent('change-dimensions', () => { 
+   calc();
+});
+
+function calc() { 
+   const res = matrixMulNum(matrixA.getData(), num);
+   matrixB.setData(res);
+}
+
+(<any>window).setNum = (_num: number) => { 
+   num = _num;
+   matrixB.title = `A * ${num}`;
+   calc();
+   // input[type=number, min, max]
+}
