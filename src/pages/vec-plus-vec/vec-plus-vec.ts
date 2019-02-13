6ef5faddb45355fc64d6ec752ff39866b5cb9b2f
DimaGashko <dgashko@mail.ua>
2019-02-13 02:55:42 +0200

    vecPlusVec

--- src/pages/vec-plus-vec/vec-plus-vec.ts
+++ src/pages/vec-plus-vec/vec-plus-vec.ts
@@ -3,12 +3,17 @@ import './vec-plus-vec.sass';
 
 import MatrixDom from '../../components/MatrixDom/MatrixDom';
 import vecPlusVec from '../../calcFunctions/vecPlusVec';
+import { mulOperator, baseOperators, Operator } from '../../calcFunctions/Operator';
+
 const workspace = document.querySelector('.calc-workspace');
 
 const els = {
    vecA: workspace.querySelector('.calc__vecA'),
    vecB: workspace.querySelector('.calc__vecB'),
    vecC: workspace.querySelector('.calc__vecC'),
+
+   selectOperator: <HTMLSelectElement>workspace
+      .querySelector('.selectOperator'),
 }
 
 const vecA = new MatrixDom({
@@ -20,7 +25,7 @@ const vecB = new MatrixDom({
    maxM: 1,
 });
 const vecC = new MatrixDom({
-   title: 'A + B',
+   title: getTitle(),
    disabled: true,
    maxM: 1,
 });
@@ -50,7 +55,27 @@ vecB.addEvent('change-dimensions', () => {
    calc();
 });
 
+els.selectOperator.addEventListener('change', () => {
+   vecB.title = getTitle();
+   calc();
+});
+
 function calc() {
-   const res = vecPlusVec(vecA.getData()[0], vecB.getData()[0]);
+   const res = vecPlusVec(vecA.getData()[0], vecB.getData()[0], getOperator());
    vecC.setData([res]);
+}
+
+function getTitle() {
+   return `A ${getOperator().symbol} B`;
+}
+
+function getOperator(): Operator {
+   const operatorStr = els.selectOperator.value;
+   let operator = mulOperator;
+
+   if (operatorStr in baseOperators) {
+      operator = baseOperators[operatorStr];
+   }
+
+   return operator;
 }
