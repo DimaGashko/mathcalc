d556bd622f280b4a54e1d590c81b7a93a6bc7f6a
DimaGashko <dgashko@mail.ua>
2019-02-13 02:43:08 +0200

    matrix-mul-num

--- src/pages/matrix-mul-num/matrix-mul-num.ts
+++ src/pages/matrix-mul-num/matrix-mul-num.ts
@@ -3,26 +3,30 @@ import './matrix-mul-num.sass';
 
 import MatrixDom from '../../components/MatrixDom/MatrixDom';
 import matrixMulNum from '../../calcFunctions/matrixMulNum';
+import { Operator, baseOperators, mulOperator } from '../../calcFunctions/Operator';
 const workspace = document.querySelector('.calc-workspace');
 
 const els = {
    matrixA: workspace.querySelector('.calc__matrixA'),
    matrixB: workspace.querySelector('.calc__matrixB'),
+
+   selectOperator: <HTMLSelectElement>workspace
+      .querySelector('.selectOperator'),
+   
+   selectNumber: <HTMLInputElement>workspace
+      .querySelector('.selectNumber'),
 }
 
-console.log('Matrix multiplication');
- 
-let num = 5;
+const minNum = +els.selectNumber.min || 1e6;
+const maxNum = +els.selectNumber.max || 1e6;
 
 const matrixA = new MatrixDom({
    title: 'Matrix A',
 });
 const matrixB = new MatrixDom({
-   title: `A * ${num}`,
+   title: getTitle(),
    disabled: true,
 });
-
-(<any>window).c = matrixB;
 
 els.matrixA.appendChild(matrixA.root);
 els.matrixB.appendChild(matrixB.root);
@@ -38,14 +42,57 @@ matrixA.addEvent('change-dimensions', () => {
    calc();
 });
 
+els.selectNumber.addEventListener('change', () => {
+   matrixB.title = getTitle();
+   calc();
+});
+
+els.selectNumber.addEventListener('keyup', (event) => {
+   matrixB.title = getTitle();
+   calc();
+
+   if (event.keyCode === 13) {
+      els.selectNumber.value = getNum() + '';
+   }
+});
+
+els.selectNumber.addEventListener('blur', () => {
+   els.selectNumber.value = getNum() + '';
+});
+
+els.selectOperator.addEventListener('change', () => {
+   matrixB.title = getTitle();
+   calc();
+});
+
 function calc() { 
-   const res = matrixMulNum(matrixA.getData(), num);
+   const res = matrixMulNum(matrixA.getData(), getNum(), getOperator());
    matrixB.setData(res);
 }
 
-(<any>window).setNum = (_num: number) => { 
-   num = _num;
-   matrixB.title = `A * ${num}`;
-   calc();
-   // input[type=number, min, max]
+function getTitle() {
+   let num = getNum();
+   let strNum = (num > 0) ? num + '' : `(${num})`;
+
+   return `A ${getOperator().symbol} ${strNum}`;
+}
+ 
+function getNum(): number {
+   let num = +els.selectNumber.value || 0;
+
+   if (num < minNum) num = minNum;
+   else if (num > maxNum) num = maxNum;
+
+   return num;
+}
+
+function getOperator(): Operator {
+   const operatorStr = els.selectOperator.value;
+   let operator = mulOperator;
+
+   if (operatorStr in baseOperators) {
+      operator = baseOperators[operatorStr];
+   }
+
+   return operator;
 }
